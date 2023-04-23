3bbb15b760f128decdab6d012c4069bcaf7cc634
aleksandre019 <128220144+aleksandre019@users.noreply.github.com>
2023-04-24 00:55:58 +0400

    Add files via upload
    1

---
+++ midterm.py
@@ -0,0 +1,17 @@
+# 1.
+# import random
+# def random_input():
+#     random_num= random.randint(0, 1)
+#     user_input= input("sheiyvane sityva:")
+#     input_lenght = len(user_input)
+#     print(random_num)
+#     if random_num == 1:
+#         new_string = user_input.replace('y','')
+#         print("shecvlili sityva:", new_string)
+#     else:
+#        if 'o' in user_input:
+#            print("stringi sheicavs 'o' ")
+#        else:
+#            print("stringi  ar sheicavs 'o' ")
+#     print('stiqonis sigrdze:', input_lenght)
+# random_input()
